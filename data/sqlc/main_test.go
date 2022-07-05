ac72edd567279d10edbd721a2e3a25e07b916498
yunusozdemirr <yunusozdemir468@gmail.com>
2022-07-05 13:55:11 +0300

    The first unit test is successfully worked

--- data/sqlc/main_test.go
+++ data/sqlc/main_test.go
@@ -2,11 +2,24 @@ package db
 
 import (
 	"database/sql"
+	"log"
 	"testing"
+
+	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
 func TestMain(m *testing.M) {
 	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect db")
+	}
+	testQueries = New(conn)
+	m.Run()
 }
